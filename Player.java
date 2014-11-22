d4a865885847b0264bb0ce12440089c2ac486759
Michael Shum <m1shum@ucsd.edu>
2014-11-22 00:10:04 -0800

    Updated Player.java for the 10th time

--- Player.java
+++ Player.java
@@ -4,32 +4,37 @@ import java.lang.Integer;
 
 public class Player
 {
-    final int MONEY_DEFAULT = 90000;
-    final int MONEY_INCOME  =  10000;
+    final int MONEY_DEFAULT_SAVINGS = 90000;
+    final int MONEY_DEFAULT_INCOME  =  10000;
     final int NUM_OF_PARTS = 5;
 
     final int LUCK_MIN = 1;
     final int LUCK_MAX = 100;
 
     int bigMoney;
+    int income;
     int luck;
-    /*  Nuclear Core = shipParts[0]
-     *  Wing Set = shipParts[1]
-     *  Thursters = shipParts[2]
-     *  Life Support = shipParts[3]
-     *  Cock Pit = shipParts[4]
+    /*  Nuclear Core = shipParts[0] | 0
+     *  Wing Set = shipParts[1]     | 1
+     *  Thursters = shipParts[2]    | 2
+     *  Life Support = shipParts[3] | 3
+     *  Cock Pit = shipParts[4]     | 4
      */
     
-    ArrayList<Integer>[] shipParts = new ArrayList[ NUM_OF_PARTS ];
+    //ArrayList<Integer>[] shipParts = new ArrayList[ NUM_OF_PARTS ];
+    ArrayList<Integer> shipParts = new ArrayList<Integer>();
+    
     Random rand = new Random();
+    /* THIS IS WHEN THERE IS 'QUALITY' IMPLEMENTED!
     //initialize
     public Player()
     {
         // initialize array thing
         for( int i = 0; i < shipParts.length; i++ )
             shipParts[i] = new ArrayList<Integer>();
-        
+        salary = MONEY_INCOME;
         bigMoney = MONEY_DEFAULT;
+
     }
 
     public Player( int initMoney )
@@ -52,19 +57,52 @@ public class Player
     boolean removePart(int part, int val) {
         return shipParts[part].remove( new Integer(val) );
     }
+    */
+
+    //initialize
+    public Player()
+    {
+        income = MONEY_DEFAULT_INCOME;
+        bigMoney = MONEY_DEFAULT_SAVINGS;
+
+    }
+
+    boolean addPart( int part )
+    {
+        if( shipParts.contains( new Integer(part) ) )
+            return false;
+        shipParts.add( new Integer(part) );
+        return true;
+    }
+
+    boolean isPart( int part )
+    {
+        return shipParts.contains( new Integer(part) );
+    }
+
+    boolean removePart( int part )
+    {
+        return shipParts.remove( new Integer(part) );
+    }
+
+    public Player( int sav, int inc )
+    {
+        income = inc;
+        bigMoney = sav;
+    }
 
     int nextDay() {
-        bigMoney += MONEY_INCOME;
+        bigMoney += income;
         luck = rand.nextInt(LUCK_MAX) + LUCK_MIN;
         return bigMoney; 
     }
 
-    int addMoney( int gettinMoney ) {
+    int addSavings( int gettinMoney ) {
         bigMoney += gettinMoney;
         return bigMoney;
     }
 
-    int subMoney( int losinMoney ) {
+    int subSavings( int losinMoney ) {
         //pick up yo hussle game
         //or die on earth
         bigMoney -= losinMoney;
@@ -74,5 +112,22 @@ public class Player
     int getLuck()
     {
         return luck;
+    }
+
+    int getIncome()
+    {
+        return income;
+    }
+
+    int addIncome( int gettinMoney )
+    {
+        income += gettinMoney;
+        return income;
+    }
+
+    int subIncome( int losinMoney )
+    {
+        income -= losinMoney;
+        return income;
     }
 }
