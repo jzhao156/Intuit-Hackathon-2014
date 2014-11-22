62265685b594c3f67e2657c3205a8ed8e5ca6cbe
Yue Zhao <yuz217@ucsd.edu>
2014-11-21 21:52:12 -0800

    Label on top, picture in middle, buttons on bottom

--- GameController.java
+++ GameController.java
@@ -2,14 +2,19 @@ import java.util.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.event.*;
 import java.applet.*;
 
 public class GameController extends JApplet implements ActionListener//, Mouselistener
 {
+    public int daysLeft, moneyCnt;
+    private JPanel bigPanel = new JPanel();
     private JPanel bottomPanel = new JPanel(new FlowLayout());
+    private JPanel topPanel = new JPanel(new FlowLayout());
     private JPanel imagePanel = new JPanel();
     private JButton shopButton;
     private JButton helpButton;
+    private JLabel days, money;
     
     public void init ()
     {
@@ -19,20 +24,41 @@ public class GameController extends JApplet implements ActionListener//, Mouseli
         shopButton = new JButton("Shop");
         helpButton = new JButton("Help");
         
+        shopButton.addActionListener(this);
+        helpButton.addActionListener(this);
+        
+        shopButton.setPreferredSize( new Dimension(300, 75));
+        helpButton.setPreferredSize( new Dimension(300, 75));
+        
+        days = new JLabel("days left: " + daysLeft);
+        money = new JLabel( "money" + "$" + moneyCnt);
+        
         bottomPanel.add(test);
         bottomPanel.add(shopButton);
         bottomPanel.add(helpButton);
+        topPanel.add(days);
+        topPanel.add(money);
         
-        add(bottomPanel);
+        bigPanel.setLayout( new BoxLayout(bigPanel,BoxLayout.Y_AXIS));
+        bigPanel.add(topPanel);
+        bigPanel.add(bottomPanel);
         
-        shopButton.addActionListener(this);
-        helpButton.addActionListener(this);
+        add(bigPanel);
+        
+        this.validate();
         
     }
     
     public void actionPerformed(ActionEvent evt)
     {
         //TODO
-        System.out.println("Poop");
+        if (evt.getSource() == shopButton)
+        {
+            System.out.println("Clicked the shop");
+        }
+        if (evt.getSource() == helpButton)
+        {
+            System.out.println("Clicked help");
+        }
     }
 }
