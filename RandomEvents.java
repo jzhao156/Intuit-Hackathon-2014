9f405051954f511abf6f6fb0dc2fb7f34cd8d438
Michael Shum <m1shum@ucsd.edu>
2014-11-22 06:31:15 -0800

    added xxactionxx abstract class #1

---
+++ RandomEvents.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class RandomEvents extends xxActioNxx
+{
+    private String event;
+    
+    public RandomEvents( String ev, ArrayList<toDo> arg2 )
+    {
+        toDoArray = arg2;
+        event = ev;
+    }
+    
+    public ArrayList<toDo> returnToDoArray()
+    {
+        return toDoArray;
+    }
+}
---
+++ xxActioNxx.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public abstract class xxActioNxx
+{
+    ArrayList<toDo> toDoArray;
+
+    abstract ArrayList<toDo> returnToDoArray();
+
+
+
+}
