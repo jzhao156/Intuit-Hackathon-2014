9ee2f2724017648820dfaaf557aa4f15bf80e1dc
Yue Zhao <yuz217@ucsd.edu>
2014-11-21 19:46:12 -0800

    Made view controller and html for applet

---
+++ GameController.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.applet.*;
+
+public class GameController extends JApplet //implements MouseListener, ActionListener
+{
+public void init ()
+    {
+        JLabel lbl = new JLabel("Hello World");
+        add(lbl);
+    }
+}
